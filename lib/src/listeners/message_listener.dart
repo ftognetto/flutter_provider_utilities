d863985cee77aac7ad537bde42c32babb0f1e5d3
fabriziotognetto <info@quantos.it>
2020-11-27 00:14:11 +0100

    adding some info

--- lib/src/listeners/message_listener.dart
+++ lib/src/listeners/message_listener.dart
@@ -6,19 +6,47 @@ import 'package:provider/provider.dart';
 import 'package:provider_utilities/src/mixin/message_notifier_mixin.dart';
 import 'package:tuple/tuple.dart';
 
+/// A listener for [ChangeNotifier] that extends [MessageNotifierMixin] mixin
+/// Wrapping a widget with [MessageListener] will use [Scaffold.context] to show Snackbars called from the ChangeNotifier class with [notifyError] or [notifyInfo] methods
+/// As an example:
+/// ```dart
+/// ChangeNotifierProvider.value(
+///   value: _model,
+///   child: Scaffold(
+///    appBar: AppBar(),
+///    body: MessageListener<Model>(
+///       child: ListView()
+///    )
+///   )
+/// );
 class MessageListener<T extends MessageNotifierMixin> extends StatelessWidget {
 
+
   final Widget child;
+
+  /// Additional function that can be called when an error message occur
   final void Function(String error) onError;
 
+  /// if [onErrorTap] is not null an action will be added to the [SnackBar] when an error message occur
   final void Function() onErrorTap;
+
+  /// Customize error [SnackBar] action label
   final String errorActionLabel;
+
+  /// Customize error [SnackBar] action color
   final Color errorActionLabelColor;
 
+  /// if [onInfoTap] is not null an action will be added to the [SnackBar] when an info message occur
   final void Function() onInfoTap;
+  
+  /// Customize info [SnackBar] action label
   final String infoActionLabel;
+
+  /// Customize info [SnackBar] action color
   final Color infoActionLabelColor;
 
+  /// [SnackBar] duration
+  /// default is Duration(milliseconds: 4000)
   final Duration snackBarDisplayTime;
 
   const MessageListener({Key key, @required this.child, this.onError, this.onErrorTap, this.errorActionLabel = 'Segnala', this.errorActionLabelColor = Colors.white, this.onInfoTap, this.infoActionLabel = 'Info', this.infoActionLabelColor = Colors.white, this.snackBarDisplayTime = const Duration(milliseconds: 4000)}) : super(key: key);
