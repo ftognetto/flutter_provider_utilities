17766e2103fbfe802a65f3106df11ceddde7479a
BL <17673738+BlazL@users.noreply.github.com>
2022-06-14 11:57:16 +0200

    some changes

--- lib/src/listeners/message_overlay_listener.dart
+++ lib/src/listeners/message_overlay_listener.dart
@@ -1,7 +1,4 @@
-
-
 import 'dart:math';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_provider_utilities/src/listeners/message_listener.dart';
 import 'package:flutter_provider_utilities/src/mixin/message_notifier_mixin.dart';
@@ -9,7 +6,7 @@ import 'package:flutter_provider_utilities/src/mixin/message_notifier_mixin.dart
 /// A listener for [ChangeNotifier] that extends [MessageNotifierMixin] mixin
 /// Wrapping a widget with [MessageListener] will use [Scaffold.context] to show Snackbars called from the ChangeNotifier class with [notifyError] or [notifyInfo] methods
 /// Useful to display error or information messages
-/// 
+///
 /// As an example:
 /// ```dart
 /// ChangeNotifierProvider.value(
@@ -22,9 +19,8 @@ import 'package:flutter_provider_utilities/src/mixin/message_notifier_mixin.dart
 ///   )
 /// );
 /// ```
-class MessageOverlayListener<T extends MessageNotifierMixin> extends StatefulWidget {
-
-
+class MessageOverlayListener<T extends MessageNotifierMixin>
+    extends StatefulWidget {
   final Widget child;
 
   /// Additional function that can be called when an error message occur
@@ -58,7 +54,7 @@ class MessageOverlayListener<T extends MessageNotifierMixin> extends StatefulWid
   /// Customize info [Overlay] leading
   /// default to Icons.info
   final Widget infoLeading;
-  
+
   /// Customize info [Overlay] trailing widget
   /// Default is empty
   final Widget? infoTrailing;
@@ -70,26 +66,38 @@ class MessageOverlayListener<T extends MessageNotifierMixin> extends StatefulWid
   /// Customize info [Overlay] text color
   /// default to Colors.white
   final Color infoColor;
-  
-  const MessageOverlayListener({
-    Key? key, 
-    required this.child, 
-    this.onError, this.onErrorTap, this.errorBackgroundColor = Colors.red, this.errorColor = Colors.white, this.errorLeading = const Icon(Icons.error, color: Colors.white), this.errorTrailing,
-    this.onInfo, this.onInfoTap, this.infoBackgroundColor = Colors.lightBlue, this.infoColor = Colors.white, this.infoLeading = const Icon(Icons.info, color: Colors.white), this.infoTrailing
-  }) : super(key: key);
+
+  const MessageOverlayListener(
+      {Key? key,
+      required this.child,
+      this.onError,
+      this.onErrorTap,
+      this.errorBackgroundColor = Colors.red,
+      this.errorColor = Colors.white,
+      this.errorLeading = const Icon(Icons.error, color: Colors.white),
+      this.errorTrailing,
+      this.onInfo,
+      this.onInfoTap,
+      this.infoBackgroundColor = Colors.lightBlue,
+      this.infoColor = Colors.white,
+      this.infoLeading = const Icon(Icons.info, color: Colors.white),
+      this.infoTrailing})
+      : super(key: key);
 
   @override
-  _MessageOverlayListenerState<T> createState() => _MessageOverlayListenerState();
+  _MessageOverlayListenerState<T> createState() =>
+      _MessageOverlayListenerState();
 }
 
-class _MessageOverlayListenerState<T extends MessageNotifierMixin> extends State<MessageOverlayListener<T>> with SingleTickerProviderStateMixin<MessageOverlayListener<T>> {
-
+class _MessageOverlayListenerState<T extends MessageNotifierMixin>
+    extends State<MessageOverlayListener<T>>
+    with SingleTickerProviderStateMixin<MessageOverlayListener<T>> {
   OverlayEntry? _notificationPopup;
   AnimationController? controller;
   final animationDuration = Duration(milliseconds: 400);
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     controller = AnimationController(vsync: this, duration: animationDuration);
   }
@@ -97,68 +105,66 @@ class _MessageOverlayListenerState<T extends MessageNotifierMixin> extends State
   @override
   Widget build(BuildContext context) {
     return MessageListener<T>(
-      child: widget.child, 
-      showError: (error) => _handleError(context, error), 
-      showInfo: (info) => _handleInfo(context, info)
-    );
+        child: widget.child,
+        showError: (error) => _handleError(context, error),
+        showInfo: (info) => _handleInfo(context, info));
   }
 
   void _handleError(BuildContext context, String error) {
-    _close().then((_){
+    _close().then((_) {
       _notificationPopup = OverlayEntry(
-        builder: (context2) => _OverlayBody(
-          controller: controller, 
-          body: error,
-          leading: widget.errorLeading,
-          trailing: widget.errorTrailing,
-          backgroundColor: widget.errorBackgroundColor,
-          textColor: widget.errorColor,
-          onTap: widget.onErrorTap,
-          onClosed: (){
-            _close();
-          },
-        )
-        
-      );
+          builder: (context2) => _OverlayBody(
+                controller: controller,
+                body: error,
+                leading: widget.errorLeading,
+                trailing: widget.errorTrailing,
+                backgroundColor: widget.errorBackgroundColor,
+                textColor: widget.errorColor,
+                onTap: widget.onErrorTap,
+                onClosed: () {
+                  _close();
+                },
+              ));
       Overlay.of(context)!.insert(_notificationPopup!);
-      if (widget.onError != null) { widget.onError!(error); }
+      if (widget.onError != null) {
+        widget.onError!(error);
+      }
     });
   }
 
   void _handleInfo(BuildContext context, String info) {
-    if (ModalRoute.of(context)!.isCurrent){
+    if (ModalRoute.of(context)!.isCurrent) {
       _close().then((_) {
         _notificationPopup = OverlayEntry(
-          builder: (context2) => _OverlayBody(
-            controller: controller, 
-            body: info,
-            leading: widget.infoLeading,
-            trailing: widget.infoTrailing,
-            backgroundColor: widget.infoBackgroundColor,
-            textColor: widget.infoColor,
-            onTap: widget.onInfoTap,
-            onClosed: (){
-              _close();
-            },
-            
-          )
-        );
+            builder: (context2) => _OverlayBody(
+                  controller: controller,
+                  body: info,
+                  leading: widget.infoLeading,
+                  trailing: widget.infoTrailing,
+                  backgroundColor: widget.infoBackgroundColor,
+                  textColor: widget.infoColor,
+                  onTap: widget.onInfoTap,
+                  onClosed: () {
+                    _close();
+                  },
+                ));
         Overlay.of(context)!.insert(_notificationPopup!);
-        if (widget.onInfo != null) { widget.onInfo!(info); }
+        if (widget.onInfo != null) {
+          widget.onInfo!(info);
+        }
       });
     }
   }
 
   Future<void> _close() async {
-      if (_notificationPopup != null) {
-        _notificationPopup!.remove();
-        _notificationPopup = null;
-      }
+    if (_notificationPopup != null) {
+      _notificationPopup!.remove();
+      _notificationPopup = null;
+    }
   }
 }
 
 class _OverlayBody extends StatefulWidget {
-
   final AnimationController? controller;
   final String body;
   final Widget? leading;
@@ -168,88 +174,93 @@ class _OverlayBody extends StatefulWidget {
   final void Function(String body)? onTap;
   final void Function()? onClosed;
 
-  _OverlayBody({required this.controller, required this.body, this.leading, this.trailing, required this.backgroundColor, required this.textColor, this.onClosed, this.onTap, Key? key}) : super(key: key);
+  _OverlayBody(
+      {required this.controller,
+      required this.body,
+      this.leading,
+      this.trailing,
+      required this.backgroundColor,
+      required this.textColor,
+      this.onClosed,
+      this.onTap,
+      Key? key})
+      : super(key: key);
 
   @override
   _OverlayBodyState createState() => _OverlayBodyState();
 }
 
 class _OverlayBodyState extends State<_OverlayBody> {
-
   late Animation<double> positionAnimation;
 
-   @override
+  @override
   void initState() {
     super.initState();
-    positionAnimation = Tween<double>(begin: -48.0, end: 24.0).animate(CurvedAnimation(parent: widget.controller!, curve: Curves.linear));
+    positionAnimation = Tween<double>(begin: -48.0, end: 24.0).animate(
+        CurvedAnimation(parent: widget.controller!, curve: Curves.linear));
     widget.controller!.forward();
     widget.controller!.addListener(_refresh);
-    Future.delayed(Duration(seconds: 5),() {
+    Future.delayed(Duration(seconds: 5), () {
       _close();
     });
   }
 
-  void _refresh()  => setState(() {});
+  void _refresh() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
-
     return Positioned(
         left: 0,
         top: positionAnimation.value,
-        width: MediaQuery.of(context).size.width, 
+        width: MediaQuery.of(context).size.width,
         child: Material(
-          type: MaterialType.transparency,
-          elevation: 10,
-          child: InkWell(
-            onTap: (){ 
-              if (widget.onTap != null) { widget.onTap!(widget.body); }
-              _close(); 
-            },
-            child: Dismissible(
-            key: Key('in_app_notification_dismissible_${Random().nextDouble()}'),
-              direction: DismissDirection.up,
-              onDismissed: (direction){
-                widget.onClosed!();
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: widget.backgroundColor,
-                    borderRadius: BorderRadius.circular(16)
-                  ),
-                  
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      if (widget.leading != null) widget.leading!,
-                      if (widget.leading != null) SizedBox(width: 16),
-                      Expanded(
-                        child: Text(widget.body, style: TextStyle(color: widget.textColor))
-                      ),
-                      if (widget.trailing != null) SizedBox(width: 16),
-                      if (widget.trailing != null) widget.trailing!
-                    ],
-                  ),
-                )
-              )
-              
-            )
-          )
-        )
-      );
+            type: MaterialType.transparency,
+            elevation: 10,
+            child: InkWell(
+                onTap: () {
+                  if (widget.onTap != null) {
+                    widget.onTap!(widget.body);
+                  }
+                  _close();
+                },
+                child: Dismissible(
+                    key: Key(
+                        'in_app_notification_dismissible_${Random().nextDouble()}'),
+                    direction: DismissDirection.up,
+                    onDismissed: (direction) {
+                      widget.onClosed!();
+                    },
+                    child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: widget.backgroundColor,
+                              borderRadius: BorderRadius.circular(16)),
+                          padding: const EdgeInsets.all(16),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              if (widget.leading != null) widget.leading!,
+                              if (widget.leading != null) SizedBox(width: 16),
+                              Expanded(
+                                  child: Text(widget.body,
+                                      style:
+                                          TextStyle(color: widget.textColor))),
+                              if (widget.trailing != null) SizedBox(width: 16),
+                              if (widget.trailing != null) widget.trailing!
+                            ],
+                          ),
+                        ))))));
   }
 
   void _close() {
     widget.controller!.reverse().then((value) {
-       widget.onClosed!();
+      widget.onClosed!();
     });
   }
 
   @override
-  void dispose() { 
+  void dispose() {
     widget.controller!.removeListener(_refresh);
     super.dispose();
   }
--- lib/src/listeners/notification_listener.dart
+++ lib/src/listeners/notification_listener.dart
@@ -1,5 +1,3 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_provider_utilities/src/mixin/mixin.dart';
 import 'package:flutter_provider_utilities/src/mixin/notification_notifier_mixin.dart';
@@ -8,7 +6,7 @@ import 'package:provider/provider.dart';
 /// A listener for [ChangeNotifier] that extends [NotificationNotifierMixin] mixin
 /// Wrapping a widget with [NotificationListener] will display an [Overlay] called from the ChangeNotifier class with [notifyNotification]
 /// Useful to display in-app notifications
-/// 
+///
 /// As an example:
 /// ```dart
 /// ChangeNotifierProvider.value(
@@ -21,8 +19,8 @@ import 'package:provider/provider.dart';
 ///   )
 /// );
 /// ```
-class NotificationListener<T extends NotificationNotifierMixin<Y>, Y> extends StatefulWidget {
-
+class NotificationListener<T extends NotificationNotifierMixin<Y>, Y>
+    extends StatefulWidget {
   final Widget child;
   final Widget Function(Y message)? leadingBuilder;
   final Widget Function(Y message) titleBuilder;
@@ -30,20 +28,29 @@ class NotificationListener<T extends NotificationNotifierMixin<Y>, Y> extends St
   final Widget Function(Y message)? trailingBuilder;
   final void Function(Y message)? onTap;
 
-  const NotificationListener({Key? key, required this.child, this.leadingBuilder, required this.titleBuilder, this.bodyBuilder, this.trailingBuilder, this.onTap }): super(key: key);
+  const NotificationListener(
+      {Key? key,
+      required this.child,
+      this.leadingBuilder,
+      required this.titleBuilder,
+      this.bodyBuilder,
+      this.trailingBuilder,
+      this.onTap})
+      : super(key: key);
 
   @override
   _NotificationListenerState createState() => _NotificationListenerState();
 }
 
-class _NotificationListenerState<T extends NotificationNotifierMixin<Y>, Y> extends State<NotificationListener> with SingleTickerProviderStateMixin<NotificationListener> {
-
+class _NotificationListenerState<T extends NotificationNotifierMixin<Y>, Y>
+    extends State<NotificationListener>
+    with SingleTickerProviderStateMixin<NotificationListener> {
   OverlayEntry? _notificationPopup;
   AnimationController? controller;
   final animationDuration = Duration(milliseconds: 400);
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     controller = AnimationController(vsync: this, duration: animationDuration);
   }
@@ -51,23 +58,22 @@ class _NotificationListenerState<T extends NotificationNotifierMixin<Y>, Y> exte
   @override
   Widget build(BuildContext context) {
     return Selector<T, Y?>(
-      selector: (ctx, model) => model.notification,
-      shouldRebuild: (before, after) {
-        return before != after;
-      },
-      builder: (context, notification, child){
-        WidgetsBinding.instance!.addPostFrameCallback((_){
-          _handleNotification(context, notification); 
-        });
-        return child!;
-      },
-      child: widget.child
-    );
+        selector: (ctx, model) => model.notification,
+        shouldRebuild: (before, after) {
+          return before != after;
+        },
+        builder: (context, notification, child) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            _handleNotification(context, notification);
+          });
+          return child!;
+        },
+        child: widget.child);
   }
 
   void _handleNotification(BuildContext context, Y? notification) {
     if (notification != null) {
-      if (ModalRoute.of(context)!.isCurrent){
+      if (ModalRoute.of(context)!.isCurrent) {
         _show(notification);
         Provider.of<T>(context, listen: false).clearNotification();
       }
@@ -75,40 +81,32 @@ class _NotificationListenerState<T extends NotificationNotifierMixin<Y>, Y> exte
   }
 
   Future<void> _show(Y message) async {
-
     await _close();
     _notificationPopup = OverlayEntry(
-      builder: (context2) => _NotificationBody(
-          controller: controller, 
-          notification: message,
-          leadingBuilder: widget.leadingBuilder,
-          titleBuilder: widget.titleBuilder,
-          bodyBuilder: widget.bodyBuilder,
-          trailingBuilder: widget.trailingBuilder,
-          onTap: (dynamic message) => widget.onTap!(message),
-          onClosed: (){ 
-            _close(); 
-          },
-        )
-      
-    );
+        builder: (context2) => _NotificationBody(
+              controller: controller,
+              notification: message,
+              leadingBuilder: widget.leadingBuilder,
+              titleBuilder: widget.titleBuilder,
+              bodyBuilder: widget.bodyBuilder,
+              trailingBuilder: widget.trailingBuilder,
+              onTap: (dynamic message) => widget.onTap!(message),
+              onClosed: () {
+                _close();
+              },
+            ));
     Overlay.of(context)!.insert(_notificationPopup!);
-
   }
 
   Future<void> _close() async {
-      if (_notificationPopup != null) {
-        _notificationPopup!.remove();
-        _notificationPopup = null;
-      }
+    if (_notificationPopup != null) {
+      _notificationPopup!.remove();
+      _notificationPopup = null;
+    }
   }
-
-  
-
 }
 
 class _NotificationBody<Y> extends StatefulWidget {
-
   final AnimationController? controller;
   final Y notification;
   final Widget Function(Y notification)? leadingBuilder;
@@ -118,83 +116,93 @@ class _NotificationBody<Y> extends StatefulWidget {
   final void Function(Y message)? onTap;
   final void Function()? onClosed;
 
-  _NotificationBody({required this.controller, required this.notification, this.leadingBuilder, required this.titleBuilder, this.bodyBuilder, this.trailingBuilder, this.onClosed, this.onTap, Key? key}) : super(key: key);
+  _NotificationBody(
+      {required this.controller,
+      required this.notification,
+      this.leadingBuilder,
+      required this.titleBuilder,
+      this.bodyBuilder,
+      this.trailingBuilder,
+      this.onClosed,
+      this.onTap,
+      Key? key})
+      : super(key: key);
 
   @override
   _NotificationBodyState createState() => _NotificationBodyState();
 }
 
 class _NotificationBodyState extends State<_NotificationBody> {
-
   late Animation<double> positionAnimation;
 
-   @override
+  @override
   void initState() {
     super.initState();
-    positionAnimation = Tween<double>(begin: -48.0, end: 24.0).animate(CurvedAnimation(parent: widget.controller!, curve: Curves.linear));
+    positionAnimation = Tween<double>(begin: -48.0, end: 24.0).animate(
+        CurvedAnimation(parent: widget.controller!, curve: Curves.linear));
     widget.controller!.forward();
     widget.controller!.addListener(_refresh);
-    Future.delayed(Duration(seconds: 5),() {
+    Future.delayed(Duration(seconds: 5), () {
       _close();
     });
   }
 
-  void _refresh()  => setState(() {});
+  void _refresh() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
-
     return Positioned(
         left: 0,
         top: positionAnimation.value,
-        width: MediaQuery.of(context).size.width, 
+        width: MediaQuery.of(context).size.width,
         child: Material(
-          type: MaterialType.transparency,
-          elevation: 10,
-          child: InkWell(
-            onTap: (){ 
-              if (widget.onTap != null) { widget.onTap!(widget.notification); }
-              _close(); 
-            },
-            child: Dismissible(
-            key: Key('in_app_notification_dismissible_${widget.notification.id}'),
-              direction: DismissDirection.up,
-              onDismissed: (direction){
-                widget.onClosed!();
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  if (widget.leadingBuilder != null) widget.leadingBuilder!(widget.notification),
-                  if (widget.leadingBuilder != null) SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        widget.titleBuilder(widget.notification),
-                        if (widget.bodyBuilder != null) widget.bodyBuilder!(widget.notification)
-                      ],
-                    )
+            type: MaterialType.transparency,
+            elevation: 10,
+            child: InkWell(
+                onTap: () {
+                  if (widget.onTap != null) {
+                    widget.onTap!(widget.notification);
+                  }
+                  _close();
+                },
+                child: Dismissible(
+                  key: Key(
+                      'in_app_notification_dismissible_${widget.notification.id}'),
+                  direction: DismissDirection.up,
+                  onDismissed: (direction) {
+                    widget.onClosed!();
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      if (widget.leadingBuilder != null)
+                        widget.leadingBuilder!(widget.notification),
+                      if (widget.leadingBuilder != null) SizedBox(width: 16),
+                      Expanded(
+                          child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          widget.titleBuilder(widget.notification),
+                          if (widget.bodyBuilder != null)
+                            widget.bodyBuilder!(widget.notification)
+                        ],
+                      )),
+                      if (widget.trailingBuilder != null)
+                        widget.trailingBuilder!(widget.notification)
+                    ],
                   ),
-                  if (widget.trailingBuilder != null) widget.trailingBuilder!(widget.notification)
-                ],
-              ),
-              
-            )
-          )
-        )
-      );
+                ))));
   }
 
   void _close() {
     widget.controller!.reverse().then((value) {
-       widget.onClosed!();
+      widget.onClosed!();
     });
   }
 
   @override
-  void dispose() { 
+  void dispose() {
     widget.controller!.removeListener(_refresh);
     super.dispose();
   }
